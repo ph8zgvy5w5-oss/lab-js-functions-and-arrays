8a86b1c4e0b4d48bfeab860f978b759d2d99e9b1
Eugénie Adelin <eugenie.aimee@hotmail.fr>
2026-03-19 20:26:53 +0100

    not done but time fly

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,45 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    console.log(num1, num2)
+    if (num1 > num2)
+    return num1;
 
+    else (num1 < num2) 
+    return num2;
+}
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    let longuest = "";
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longuest.length) {
+            longuest = words[i];
+            return longuest;
+        }
+    words.splice(0, words.length);
+    }
+}
+
+//const duplicateWords = [
+  //  "mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"
+//];
+
+//function removeDuplicateWords(duplicateWords) {
+    //const uniquewords2 = [];
+
+   // duplicateWords.forEach((aWord) => {
+   // if (!uniqueduplicateWords.includes(aWords)) {
+    //        uniqueduplicateWords.push(aWord);
+   //     }
+   // });
+   // return uniqueduplicateWords;
+//}
+//console.log(removeDuplicateWords(duplicateWords))
+console.log(findLongestWord(words))
 
 
 
@@ -15,20 +47,35 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let total = 0;
+    for (let i =0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
 
+    return total;
+}
+
+console.log(sumNumbers(numbers))
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    let total = sumNumbers(numbers2)
+    return total / numbers2.length;
 
+};
 
+console.log(sumNumbers(numbers2))
+console.log(averageNumbers(numbers2))
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2) {
+    let wordToSearch = words2
+}
